99a0278ed59e702369e90668f8a4ceb34192f981
Alex4u18 <adrian_giurgi@yahoo.com>
2024-09-24 23:47:22 +0100

    adjustments

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-
+          
 function Reviews() {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
